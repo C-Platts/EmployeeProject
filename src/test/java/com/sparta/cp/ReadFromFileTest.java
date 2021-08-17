fe1c47b66445883b539fe03f0c133b4d7b5b9200
CPlatts <cplatts@spartaglobal.com>
2021-08-17 16:48:50 +0100

    Added resourced directory for test and added a blank test case

--- src/test/java/com/sparta/cp/ReadFromFileTest.java
+++ src/test/java/com/sparta/cp/ReadFromFileTest.java
@@ -1,2 +1,12 @@
-package com.sparta.cp;public class ReadFromFileTest {
+package com.sparta.cp;
+
+import org.junit.jupiter.api.Test;
+
+public class ReadFromFileTest {
+
+    @Test
+    public void testValidRecordIsRead() {
+
+    }
+
 }
