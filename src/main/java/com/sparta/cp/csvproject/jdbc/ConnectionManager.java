5d6cde277e4e0cd4efb763fc847c90be17487ee6
CPlatts <cplatts@spartaglobal.com>
2021-08-18 17:58:48 +0100

    Added a connection manager class which loads in properties and establishes a connection a dabase on localhost. Also has a method to close database

--- src/main/java/com/sparta/cp/csvproject/jdbc/ConnectionManager.java
+++ src/main/java/com/sparta/cp/csvproject/jdbc/ConnectionManager.java
@@ -1,4 +1,38 @@
 package com.sparta.cp.csvproject.jdbc;
 
+import com.sparta.cp.csvproject.jdbc.util.PropertiesLoader;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionManager {
+
+    private static Connection connection;
+
+    public Connection connectToDatabase() {
+        String url = PropertiesLoader.getProperties().getProperty("url");
+        String username = PropertiesLoader.getProperties().getProperty("userName");
+        String password = PropertiesLoader.getProperties().getProperty("password");
+
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+
+    }
+
+    public static void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
