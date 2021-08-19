78450fa5d3029de23d709053eab2b68063f12f2a
CPlatts <cplatts@spartaglobal.com>
2021-08-19 16:59:34 +0100

    Found optimal number of threads as 90 - all records successfully added

--- src/main/java/com/sparta/cp/Loader.java
+++ src/main/java/com/sparta/cp/Loader.java
@@ -15,9 +15,9 @@ import java.util.stream.Collectors;
 
 public class Loader {
 
-    private static final int THREAD_COUNT = 4;
+    private static final int THREAD_COUNT = 90;
     private final ConnectionPool pool = new ConnectionPool(THREAD_COUNT);
-    private DatabaseManager dbManager;
+    private DatabaseManager dbManager = new DatabaseManager();
     private final Thread[] threads = new Thread[THREAD_COUNT];
 
     public void start() {
@@ -25,15 +25,17 @@ public class Loader {
 
         ArrayList<EmployeeDTO> employeeList = csv.readRecords();
 
-        int segmentSize = employeeList.size() / THREAD_COUNT;
+        int segmentSize = (employeeList.size() / THREAD_COUNT + 1);
 
-        //split ListArray into NUM_THREADS segments, passing each segment into a thread
+        //split ListArray into NUM_THREADS segments, passing each segment goes to a thread
         LinkedList<List<EmployeeDTO>> segments = new LinkedList<>();
         for (int i = 0 ; i < employeeList.size(); i+= segmentSize) {
             segments.add(
-                    employeeList.subList(i, Math.min(i + segmentSize, employeeList.size() - 1))
+                    employeeList.subList(i, Math.min(i + segmentSize, employeeList.size()))
             );
         }
+
+        System.out.println(segments.size());
 
 
         DatabasePrinter.printTaskInputtingRecords();
@@ -48,7 +50,6 @@ public class Loader {
 
         for(Thread thread : threads) {
             try {
-
                 thread.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -58,11 +59,28 @@ public class Loader {
         double end = System.nanoTime();
 
         DatabasePrinter.printTaskReadingRecordCount();
-        //DatabasePrinter.printRecordCount(dbManager.getRecordCount());
-        System.out.println("Time taken to input records: " + ((end - start) / 1000_000_000));
+
+        DatabasePrinter.printRecordCount(dbManager.getRecordCount());
+        System.out.println("\n Time taken to input records: " + ((end - start) / 1000_000_000));
 
         //Time benchmark = 110.35 seconds (Large file)
         //2 threads = 83.23 seconds
+        //4 threads = 44.21 seconds
+        //8 threads = 24.62
+        //9 threads = 23.08
+        //10 threads = (1st): 25.92, (2nd): 20.92
+        //11 threads = 20.74
+        //12 threads = 19.12
+        //24 threads = 11.94
+        //32 threads = 10.38
+        //64 threads = 8.17
+        //70 threads = 8.00
+        //80 threads = 7.86
+        //90 threads = 7.39 <---Optimal
+        //91 threads = (1st): 7.61, (2nd): 7.81
+        //92 threads = (1st): 8.12, (2nd): 7.73
+        //100 threads =
+        //128 threads = 13.32
     }
 
 }
