b5a78b08e6aedff220564f0430d4b4e9a4a62904
CPlatts <cplatts@spartaglobal.com>
2021-08-20 09:34:57 +0100

    Added class for testing concurrency

---
+++ src/test/java/com/sparta/cp/concurrency/ConcurrencyTests.java
@@ -0,0 +1,18 @@
+package com.sparta.cp.concurrency;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class ConcurrencyTests {
+
+    @BeforeAll
+    static void setup() {
+
+    }
+
+    @Test
+    public void stakeholder() {
+
+    }
+
+}
