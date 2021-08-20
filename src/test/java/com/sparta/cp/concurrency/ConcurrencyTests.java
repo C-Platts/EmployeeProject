1314b0b147efa9094156cbddf575bf37d7e51b60
CPlatts <cplatts@spartaglobal.com>
2021-08-20 11:41:38 +0100

    Added speed test for 1 vs multiple threads

--- src/test/java/com/sparta/cp/concurrency/ConcurrencyTests.java
+++ src/test/java/com/sparta/cp/concurrency/ConcurrencyTests.java
@@ -1,12 +1,16 @@
 package com.sparta.cp.concurrency;
 
 import com.sparta.cp.csvproject.dto.EmployeeDTO;
+import com.sparta.cp.csvproject.jdbc.DatabaseManager;
 import com.sparta.cp.csvproject.jdbc.concurrency.ConnectionPool;
 import com.sparta.cp.csvproject.jdbc.concurrency.ThreadPool;
+import com.sparta.cp.csvproject.jdbc.util.ListSplitter;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ConcurrencyTests {
@@ -14,33 +18,106 @@ public class ConcurrencyTests {
     private static ThreadPool threadPool;
     private static ConnectionPool connectionPool;
     private static List<EmployeeDTO> employeeList;
-    private static String[] recordList;
+    private static ArrayList<String[]> recordList;
+    private static DatabaseManager dbManager;
+    private static int THREAD_COUNT;
+    private static LinkedList<List<EmployeeDTO>> segments;
 
     @BeforeAll
     static void setup() {
 
-        final int THREAD_COUNT = 1;
+        dbManager = new DatabaseManager();
 
-        threadPool = new ThreadPool(THREAD_COUNT);
-        connectionPool = new ConnectionPool(THREAD_COUNT);
 
-        recordList = new String[]
-                {
-                        "1, Mr, Test, A, First, M, tFirst@email.com, 1/11/1111, 2/22/2222, 12345",
-                        "2, Mr, Test, B, Second, M, tSecond@email.com, 1/11/1111, 2/22/2222, 12345",
-                        "3, Mr, Test, C, Third, M, tThird@email.com, 1/11/1111, 2/22/2222, 12345",
-                        "4, Mr, Test, D, Fourth, M, tFourth@email.com, 1/11/1111, 2/22/2222, 12345",
-                        "5, Mr, Test, E, Fifth, M, tFifth@email.com, 1/11/1111, 2/22/2222, 12345"
-                };
+
+        recordList = new ArrayList<String[]>();
+        recordList.add(
+                new String[]
+                        {"1", "Mr", "Test", "A", "First", "M", "tFirst@email.com", "1/11/1111", "2/22/2222", "12345"}
+        );
+        recordList.add(
+                new String[]
+                        {"2", "Mr", "Test", "A", "First", "M", "tFirst@email.com", "1/11/1111", "2/22/2222", "12345"}
+        );
+        recordList.add(
+                new String[]
+                        {"3", "Mr", "Test", "A", "First", "M", "tFirst@email.com", "1/11/1111", "2/22/2222", "12345"}
+        );
+        recordList.add(
+                new String[]
+                        {"4", "Mr", "Test", "A", "First", "M", "tFirst@email.com", "1/11/1111", "2/22/2222", "12345"}
+        );
+        recordList.add(
+                new String[]
+                        {"5", "Mr", "Test", "A", "First", "M", "tFirst@email.com", "1/11/1111", "2/22/2222", "12345"}
+        );
+        recordList.add(
+                new String[]
+                        {"6", "Mr", "Test", "A", "First", "M", "tFirst@email.com", "1/11/1111", "2/22/2222", "12345"}
+        );
+        recordList.add(
+                new String[]
+                        {"7", "Mr", "Test", "A", "First", "M", "tFirst@email.com", "1/11/1111", "2/22/2222", "12345"}
+        );
+        recordList.add(
+                new String[]
+                        {"8", "Mr", "Test", "A", "First", "M", "tFirst@email.com", "1/11/1111", "2/22/2222", "12345"}
+        );
+        recordList.add(
+                new String[]
+                        {"9", "Mr", "Test", "A", "First", "M", "tFirst@email.com", "1/11/1111", "2/22/2222", "12345"}
+        );
 
         employeeList = new ArrayList<>();
-        employeeList.add(new EmployeeDTO(recordList));
+
+        for(String[] record : recordList) {
+            employeeList.add(new EmployeeDTO(record));
+        }
 
     }
 
     @Test
-    public void placeholder() {
+    public void testMultipleThreadsAreFasterThanOne() {
 
+        dbManager.truncateTable();
+
+        setupThreads(1);
+        double singleThreadTime = getTime();
+
+        dbManager.truncateTable();
+
+        setupThreads(8);
+        double multiThreadTime = getTime();
+
+        dbManager.truncateTable();
+
+        Assertions.assertTrue(multiThreadTime < singleThreadTime);
+
+    }
+
+    private double getTime() {
+        double start = System.nanoTime();
+        threadPool.start();
+
+        try {
+            threadPool.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        double end = System.nanoTime();
+
+        return end - start;
+    }
+
+    private void setupThreads(int numThreads) {
+        threadPool = new ThreadPool(numThreads);
+        connectionPool = new ConnectionPool(numThreads);
+
+        segments = ListSplitter.split(employeeList, (employeeList.size() / numThreads));
+
+        for(int i = 0; i < numThreads; i ++) {
+            threadPool.initialiseThread(segments.get(i), connectionPool.getConnection(), i);
+        }
     }
 
 }
